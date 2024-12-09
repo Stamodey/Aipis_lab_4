d2b08117bc444e3198309354e88bf36dfe4d19e3
Stamodey <75629745+Stamodey@users.noreply.github.com>
2024-12-09 06:17:47 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-//Эта программа приветствует пользователя по имени
+
+// This program greets the user by name
 int main() {
     std::string name;
-    std::cout << "Enter your name: "; //Просим пользователя ввести его имя
-    std::cin >> name;// считываем имя пользователя
-    std::cout << "Hello world from " << name << std::endl; // приветствуем пользователя
+    std::cout << "Enter your name: "; // Ask user for their name
+    std::cin >> name; // Read user's name
+    std::cout << "Hello world from " << name << std::endl; // Greet the user
     return 0;
 }
 
